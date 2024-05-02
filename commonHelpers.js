8ff8c9a301f206593e6829586455583de9d8dcb5
shpylovyi1007 <154367584+shpylovyi1007@users.noreply.github.com>
2024-05-02 19:04:14 +0000

    Deploying to gh-pages from @ shpylovyi1007/goit-js-hw-11@3c3e23a49596d00ce1a2f6c6ba633f03d4705be0 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,12 +1,15 @@
-import{S as i,i as c}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const l=document.querySelector("form"),u=l.elements.search,d=new URLSearchParams({key:"43654443-0aa2ac279d0400a8e119738dd",q:u.value,image_type:"photo",orientation:"horizontal",safesearch:"true"}),f=()=>fetch(`https://pixabay.com/api/images?${d}`).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()}),m=document.querySelector(".gallery"),y=o=>{const s=o.map(r=>`< li class="gallery-item" >
-            <a class="gallery-link" href=${r.largeImageURL}>
+import{S as c,i as l}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const u=document.querySelector("form"),d=u.elements.search.value.trim(),m=new URLSearchParams({key:"43654443-0aa2ac279d0400a8e119738dd",q:d,image_type:"photo",orientation:"horizontal",safesearch:"true"}),f=`https://pixabay.com/api/images?${m}`,p=s=>fetch(f).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()}),i=document.querySelector(".gallery"),y=s=>{i.innerHTML="";const t=s.map(o=>`< li class="gallery-item" >
+            <a class="gallery-link" href=${o.largeImageURL}>
                 <img
                     class="gallery-image"
-                    url=${r.webformatURL}
-                    alt=${r.tags}
-                    <b>Likes</b>: ${r.likes}
-                    <b>Views</b>: ${r.views}
-                    <b>Comments</b>: ${r.comments}
-                    <b>Downloads</b>: ${r.downloads}
-                /></a></li >`).join("");m.insertAdjacentHTML("beforeend",s),new i(".gallery a",{captionsData:"alt",captionDelay:250})},h=document.querySelector("form");h.addEventListener("submit",o=>{o.preventDefault(),f().then(s=>y(s)).catch(s=>c.show({message:"Sorry, there are no images matching your search query. Please try again!"}))});
+                    url=${o.webformatURL}
+                    alt=${o.tags}
+                /></a>
+                 <div>
+                <p>Likes: ${o.likes}</p>
+                <p>Views: ${o.views}</p>
+                <p>Comments: ${o.comments}</p>
+                <p>Downloads: ${o.downloads}</p>
+            </div>
+                </li >`).join("");i.insertAdjacentHTML("beforeend",t),new c(".gallery a",{captionsData:"alt",captionDelay:250})},h=document.querySelector("form");h.addEventListener("submit",s=>{s.preventDefault(),p().then(t=>y(t)).catch(t=>l.show({message:"Sorry, there are no images matching your search query. Please try again!"}))});
 //# sourceMappingURL=commonHelpers.js.map
