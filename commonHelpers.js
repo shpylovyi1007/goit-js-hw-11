b4f6522b17b4f0786681435838cf4f6537df5533
shpylovyi1007 <154367584+shpylovyi1007@users.noreply.github.com>
2024-05-04 15:17:25 +0000

    Deploying to gh-pages from @ shpylovyi1007/goit-js-hw-11@4131dff6ea6d239b282578a2e8de2b34b3b267c7 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,15 +1,15 @@
-import{S as c,i as l}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const u=document.querySelector("form"),d=u.elements.search.value.trim(),f=new URLSearchParams({key:"43654443-0aa2ac279d0400a8e119738dd",q:d,image_type:"photo",orientation:"horizontal",safesearch:"true"}),m=`https://pixabay.com/api/images?${f}`,p=s=>fetch(m).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}),i=document.querySelector(".gallery"),y=s=>{i.innerHTML="";const r=s.map(o=>`<li class="gallery-item">
-            <a class="gallery-link" href=${o.largeImageURL}>
+import{S as l,i as u}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&t(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function t(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();const d=a=>{const r=`https://pixabay.com/api/?${new URLSearchParams({key:"43654443-0aa2ac279d0400a8e119738dd",q:a,image_type:"photo",orientation:"horizontal",safesearch:"true"})}`;return fetch(r).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()}).then(t=>(console.log(t),t.hits)).catch(t=>{throw t})},i=document.querySelector(".gallery"),f=a=>{i.innerHTML="";const s=a.map(r=>`<li class="gallery-item">
+            <a class="gallery-link" href=${r.largeImageURL}>
                 <img
-                    url=${o.webformatURL}
-                    alt=${o.tags}
+                    url=${r.webformatURL}
+                    alt=${r.tags}
                 />
             </a>
                  <div>
-                <p>Likes: ${o.likes}</p>
-                <p>Views: ${o.views}</p>
-                <p>Comments: ${o.comments}</p>
-                <p>Downloads: ${o.downloads}</p>
+                <p>Likes: ${r.likes}</p>
+                <p>Views: ${r.views}</p>
+                <p>Comments: ${r.comments}</p>
+                <p>Downloads: ${r.downloads}</p>
             </div>
-                </li>`).join("");i.insertAdjacentHTML("beforeend",r),new c(".gallery a",{captionsData:"alt",captionDelay:250})},h=document.querySelector("form");h.addEventListener("submit",s=>{s.preventDefault(),p().then(r=>y(r)).catch(r=>l.show({message:"Sorry, there are no images matching your search query. Please try again!"}))});
+                </li>`).join("");i.insertAdjacentHTML("beforeend",s),new l(".gallery a",{captionsData:"alt",captionDelay:250})},c=document.querySelector("form");c.addEventListener("submit",a=>{a.preventDefault();const s=c.elements.search.value.trim();s&&d(s).then(r=>f(r)).catch(r=>u.show({message:"Sorry, there are no images matching your search query. Please try again!"}))});
 //# sourceMappingURL=commonHelpers.js.map
