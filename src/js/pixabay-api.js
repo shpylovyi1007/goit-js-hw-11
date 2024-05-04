4131dff6ea6d239b282578a2e8de2b34b3b267c7
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-05-04 18:16:58 +0300

    homework11.update4

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,12 @@
-const form = document.querySelector('form');
-const userSearch = form.elements.search.value.trim();
-
-const searchParams = new URLSearchParams({
-    key: '43654443-0aa2ac279d0400a8e119738dd',
-    q: userSearch,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true'
-});
-const url = `https://pixabay.com/api/images?${searchParams}`;
-
 const userGallery = userSearch => {
+    const searchParams = new URLSearchParams({
+        key: '43654443-0aa2ac279d0400a8e119738dd',
+        q: userSearch,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true'
+    });
+    const url = `https://pixabay.com/api/?${searchParams}`;
     return fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -18,6 +14,12 @@ const userGallery = userSearch => {
             }
             return response.json();
         })
+        .then(images => {
+            console.log(images);
+            return images.hits;
+        })
+        .catch(error => {
+            throw error;
+        });
 };
-
 export default userGallery;
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-
 import userGallery from "./js/pixabay-api";
 import addImages from "./js/render-functions";
 const form = document.querySelector('form');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    userGallery()
+    const userSearch = form.elements.search.value.trim();
+    if (!userSearch) {
+        return;
+    }
+    userGallery(userSearch)
         .then(images => addImages(images))
         .catch(error => iziToast.show({
             message: 'Sorry, there are no images matching your search query. Please try again!'
